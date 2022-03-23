a72ea283df354351d59a513b183b79bf379f1f74
Gurpreet Singh <gurpreet.2000singh1912@gmail.com>
2022-03-23 17:33:14 +0530

    Add `strlen` function to standard library

--- include/std.tf
+++ include/std.tf
@@ -1,10 +1,21 @@
 #define NULL 0
 
+#define STDIN  0
 #define STDOUT 1
+#define STDERR 2
 
 #define SYSCALL_WRITE 1
 
 // Syscall wrapper functions
 func write(fd, buf, size) {
     SYSCALL_WRITE fd buf size syscall3
+}
+
+// String helper functions
+func strlen(string) {
+    0 do {
+        string @ drop
+        1 +
+    } dup string + @ NULL = 0 = while
+    mem swap &
 }
