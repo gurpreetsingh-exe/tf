ba890fb27c3c32a8d4a8d5ac72d3d7f83895ec08
Gurpreet Singh <gurpreet.2000singh1912@gmail.com>
2022-03-23 15:31:30 +0530

    Add `write` function into the standard library

--- include/std.tf
+++ include/std.tf
@@ -1,2 +1,10 @@
 #define strlen dup "\0" swap -
 
+#define STDOUT 1
+
+#define SYSCALL_WRITE 1
+
+// Syscall wrapper functions
+func write(fd, buf, size) {
+    SYSCALL_WRITE fd buf size syscall3
+}
