1894e9750bec0cc67d25846c693524e8b492bb8d
Gurpreet Singh <gurpreet.2000singh1912@gmail.com>
2022-06-13 19:47:09 +0530

    fix `loc_fmt` macro

--- src/library/panic.tf
+++ src/library/panic.tf
@@ -4,7 +4,7 @@ import std
 func:int number_to_mem(int) {
     let number;
     0 while number 0 != do {
-        dup mem 514 + swap - number 10 % 48 + write8
+        dup mem 512 + swap - number 10 % 48 + write8
         &number number 10 / write64
         1 +
     }
@@ -15,13 +15,11 @@ macro loc_fmt {
     1 here over over
 
     read64 number_to_mem()
-    mem 512 + swap
-
-    dup mem 513 + + 58 write64 2 +
-    write!
+    dup mem 513 + swap - swap
+    mem 513 + 58 write8 1 + write!
 
     8 + read64 number_to_mem()
-    mem 513 + swap 1 + write!
+    dup mem 513 + swap - swap write!
 }
 
 macro panic {
