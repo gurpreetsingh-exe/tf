bcead548b1516fa578c231642e71bd3b734e1b4e
Gurpreet Singh <gurpreet.2000singh1912@gmail.com>
2022-06-17 19:19:10 +0530

    generate elf header

--- src/bootstrap/gen/elf.py
+++ src/bootstrap/gen/elf.py
@@ -1,20 +1,38 @@
 class Elf64_Ehdr:
     def __init__(self):
-        self.e_ident = 0
-        self.e_type = 0
-        self.e_machine = 0
-        self.e_version = 0
-        self.e_entry = 0
-        self.e_phoff = 0
-        self.e_shoff = 0
-        self.e_flags = 0
-        self.e_ehsize = 0
-        self.e_phentsize = 0
-        self.e_phnum = 0
-        self.e_shentsize = 0
-        self.e_shnum = 0
-        self.e_shstrndx = 0
+        self.e_ident     = b"\x7f\x45\x4c\x46\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        self.e_type      = 2
+        self.e_machine   = 62    # Advance Micro Devices x86_64
+        self.e_version   = 1
+        self.e_entry     = 0     # Unknown
+        self.e_phoff     = 64    # Should be 64 bytes into the file
+        self.e_shoff     = 0     # Unknown
+        self.e_flags     = 0
+        self.e_ehsize    = 64    # This header is 64 bytes long
+        self.e_phentsize = 56    # Size of a program header is 56, or atleast it should be
+        self.e_phnum     = 0     # We don't know yet
+        self.e_shentsize = 64    # A section header is 64 bytes long
+        self.e_shnum     = 0     # We don't know yet
+        self.e_shstrndx  = 0     # ^^^^^^^^^^^^^^^^^
 
+    def emit_ehdr(self, gen):
+        gen.write(self.e_ident)
+        gen.write_u16(self.e_type)
+        gen.write_u16(self.e_machine)
+        gen.write_u32(self.e_version)
+        gen.write_u64(self.e_entry)
+        gen.write_u64(self.e_phoff)
+        gen.write_u64(self.e_shoff)
+        gen.write_u32(self.e_flags)
+        gen.write_u16(self.e_ehsize)
+        gen.write_u16(self.e_phentsize)
+        gen.write_u16(self.e_phnum)
+        gen.write_u16(self.e_shentsize)
+        gen.write_u16(self.e_shnum)
+        gen.write_u16(self.e_shstrndx)
+
+    def set_entry(self, gen, addr):
+        gen.write_u64_at(addr, 24)
 
 class Elf64_Phdr:
     def __init__(self):
---
+++ src/bootstrap/gen/gen.py
@@ -0,0 +1,95 @@
+from .elf import *
+
+class Gen:
+    def __init__(self, out_name):
+        self.out_name = out_name
+        self.buf = bytearray()
+        self.symbols = []
+        self.strings = []
+        self.labels = []
+
+    def gen_exec(self):
+        self.emit_header()
+        try:
+            with open(self.out_name, "wb") as f:
+                f.write(self.buf)
+        except FileExistsError:
+            with open(self.out_name, "xb") as f:
+                f.write(self.buf)
+
+    def emit_header(self):
+        ehdr = Elf64_Ehdr()
+        ehdr.emit_ehdr(self)
+        ehdr.set_entry(self, 0x401100)
+
+    def write_u8(self, n: int) -> None:
+        self.buf.append(n & 0xff)
+
+    def write_u16(self, n: int) -> None:
+        buf = bytearray()
+        buf.append(n & 0xff)
+        buf.append((n >> 8) & 0xff)
+        self.buf += buf
+
+    def write_u32(self, n: int) -> None:
+        buf = bytearray()
+        buf.append(n & 0xff)
+        buf.append((n >> 8) & 0xff)
+        buf.append((n >> 16) & 0xff)
+        buf.append((n >> 24) & 0xff)
+        self.buf += buf
+
+    def write_u64(self, n: int) -> None:
+        buf = bytearray()
+        buf.append(n & 0xff)
+        buf.append((n >> 8) & 0xff)
+        buf.append((n >> 16) & 0xff)
+        buf.append((n >> 24) & 0xff)
+        buf.append((n >> 32) & 0xff)
+        buf.append((n >> 40) & 0xff)
+        buf.append((n >> 48) & 0xff)
+        buf.append((n >> 56) & 0xff)
+        self.buf += buf
+
+    def write(self, buf: bytes) -> None:
+        self.buf += buf
+
+    def write_u8_at(self, n: int, addr: int) -> None:
+        if len(self.buf) < addr:
+            print(f"Write address exceeds the total buffer length write_u8_at, buf_size: {len(self.buf)}, addr: {addr}")
+            exit(1)
+
+        self.buf[addr] = n & 0xff
+
+    def write_u16_at(self, n: int, addr: int) -> None:
+        if len(self.buf) < addr:
+            print(f"Write address exceeds the total buffer length write_u16_at, buf_size: {len(self.buf)}, addr: {addr}")
+            exit(1)
+
+        self.buf[addr] = n & 0xff
+        self.buf[addr + 1] = (n >> 8) & 0xff
+
+    def write_u32_at(self, n: int, addr: int) -> None:
+        if len(self.buf) < addr:
+            print(f"Write address exceeds the total buffer length write_u32_at, buf_size: {len(self.buf)}, addr: {addr}")
+            exit(1)
+
+        self.buf[addr] = n & 0xff
+        self.buf[addr + 1] = (n >> 8) & 0xff
+        self.buf[addr + 2] = (n >> 16) & 0xff
+        self.buf[addr + 3] = (n >> 24) & 0xff
+
+    def write_u64_at(self, n: int, addr: int) -> None:
+        if len(self.buf) < addr:
+            print(f"Write address exceeds the total buffer length write_u64_at, buf_size: {len(self.buf)}, addr: {addr}")
+            exit(1)
+
+        self.buf[addr] = n & 0xff
+        self.buf[addr + 1] = (n >> 8) & 0xff
+        self.buf[addr + 2] = (n >> 16) & 0xff
+        self.buf[addr + 3] = (n >> 24) & 0xff
+        self.buf[addr + 4] = (n >> 32) & 0xff
+        self.buf[addr + 5] = (n >> 40) & 0xff
+        self.buf[addr + 6] = (n >> 48) & 0xff
+        self.buf[addr + 7] = (n >> 56) & 0xff
+
