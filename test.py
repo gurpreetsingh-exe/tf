9a574a5203d6da04061c384e03cc0ee5201e0c12
Gurpreet Singh <gurpreet.2000singh1912@gmail.com>
2022-06-25 20:47:36 +0530

    test: update tests for testing native backend

--- test.py
+++ test.py
@@ -9,9 +9,6 @@ test_dir = os.path.join(proj_dir, "tests")
 logs_file = os.path.join(proj_dir, "tests.log")
 
 state = lambda: None
-state.passed_tests = 0
-state.failed_tests = 0
-state.compile_fail = 0
 
 try:
     with open(logs_file, "w") as fd:
@@ -20,50 +17,113 @@ except FileNotFoundError:
     with open(logs_file, "x") as fd:
         pass
 
-for path in pathlib.Path(test_dir).iterdir():
-    if path.suffix != ".tf" or path.stem.startswith("mod_"):
-        continue
-    file_path = str(path.absolute())
-    file_name = path.name
-    compiler_out = ""
-    print(f"INFO: Testing {file_name}")
+def test_nasm_backend():
+    state.passed_tests = 0
+    state.failed_tests = 0
+    state.compile_fail = 0
 
-    print(f"INFO: Compiling {file_path}")
-    exec_name = file_name.split(".")[0]
-    proc = subprocess.Popen(["./src/bootstrap/tf.py", "-c", file_path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    lines = "".join([i.decode('utf-8') for i in proc.stdout.readlines()])
-    proc.communicate()
-    if proc.returncode != 0:
-        state.compile_fail += 1
-        print("ERROR: Compilation failed")
-        with open(logs_file, "a") as fd:
-            fd.write(f"== ERROR: compilation failed for {file_path}\n== ")
-            fd.write(lines)
-            fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
-        continue
+    print("INFO: Testing nasm backend")
+    for path in pathlib.Path(test_dir).iterdir():
+        if path.suffix != ".tf" or path.stem.startswith("mod_"):
+            continue
+        file_path = str(path.absolute())
+        file_name = path.name
+        print(f"INFO: Testing {file_name}")
 
-    target = os.path.join(test_dir, exec_name)
-    print(f"INFO: Running {target}")
-    proc = subprocess.Popen([target], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    lines = "".join([i.decode('utf-8') for i in proc.stdout.readlines()])
-    proc.communicate()
-    if proc.returncode != 0:
-        state.failed_tests += 1
-        print("ERROR: Execution failed")
-        with open(logs_file, "a") as fd:
-            fd.write(f"== ERROR: execution failed for {target}\n== ")
-            fd.write(lines)
-            fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
-    else:
-        state.passed_tests += 1
-        with open(logs_file, "a") as fd:
-            if proc.returncode == 101:
-                fd.write(f"[THREAD PANIC]\n")
-            fd.write(f"== INFO: Output for {target}\n== ")
-            fd.write(lines)
-            fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
-    subprocess.call(["rm", target, target + ".asm", target + ".o"])
+        print(f"INFO: Compiling {file_path}")
+        exec_name = file_name.split(".")[0]
+        proc = subprocess.Popen(["./src/bootstrap/tf.py", "-c", file_path, "-be", "nasm"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        lines = "".join([i.decode('utf-8') for i in proc.stdout.readlines()])
+        proc.communicate()
+        if proc.returncode != 0:
+            state.compile_fail += 1
+            print("ERROR: Compilation failed")
+            with open(logs_file, "a") as fd:
+                fd.write(f"== ERROR: compilation failed for {file_path}\n== ")
+                fd.write(lines)
+                fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
+            continue
 
-print(f"\n    passed tests:    {state.passed_tests}\t")
-print(f"    failed tests:    {state.failed_tests}\t")
-print(f"    compile fail:    {state.compile_fail}\t\n")
+        target = os.path.join(test_dir, exec_name)
+        print(f"INFO: Running {target}")
+        proc = subprocess.Popen([target], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        lines = "".join([i.decode('utf-8') for i in proc.stdout.readlines()])
+        proc.communicate()
+        if proc.returncode != 0:
+            state.failed_tests += 1
+            print("ERROR: Execution failed")
+            with open(logs_file, "a") as fd:
+                fd.write(f"== ERROR: execution failed for {target}\n== ")
+                fd.write(lines)
+                fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
+        else:
+            state.passed_tests += 1
+            with open(logs_file, "a") as fd:
+                if proc.returncode == 101:
+                    fd.write(f"[THREAD PANIC]\n")
+                fd.write(f"== INFO: Output for {target}\n== ")
+                fd.write(lines)
+                fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
+        subprocess.call(["rm", target, target + ".asm", target + ".o"])
+
+    print(f"\n    passed tests:    {state.passed_tests}\t")
+    print(f"    failed tests:    {state.failed_tests}\t")
+    print(f"    compile fail:    {state.compile_fail}\t\n")
+
+
+def test_native_backend():
+    state.passed_tests = 0
+    state.failed_tests = 0
+    state.compile_fail = 0
+
+    print("INFO: Testing native backend")
+    for path in pathlib.Path(test_dir).iterdir():
+        if path.suffix != ".tf" or path.stem.startswith("mod_"):
+            continue
+        file_path = str(path.absolute())
+        file_name = path.name
+        print(f"INFO: Testing {file_name}")
+
+        print(f"INFO: Compiling {file_path}")
+        exec_name = file_name.split(".")[0]
+        proc = subprocess.Popen(["./src/bootstrap/tf.py", "-c", file_path, "-be", "native"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        lines = "".join([i.decode('utf-8') for i in proc.stdout.readlines()])
+        proc.communicate()
+        if proc.returncode != 0:
+            state.compile_fail += 1
+            print("ERROR: Compilation failed")
+            with open(logs_file, "a") as fd:
+                fd.write(f"== ERROR: compilation failed for {file_path}\n== ")
+                fd.write(lines)
+                fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
+            continue
+
+        target = os.path.join(test_dir, exec_name)
+        print(f"INFO: Running {target}")
+        proc = subprocess.Popen([target], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        lines = "".join([i.decode('utf-8') for i in proc.stdout.readlines()])
+        proc.communicate()
+        if proc.returncode != 0:
+            state.failed_tests += 1
+            print("ERROR: Execution failed")
+            with open(logs_file, "a") as fd:
+                fd.write(f"== ERROR: execution failed for {target}\n== ")
+                fd.write(lines)
+                fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
+        else:
+            state.passed_tests += 1
+            with open(logs_file, "a") as fd:
+                if proc.returncode == 101:
+                    fd.write(f"[THREAD PANIC]\n")
+                fd.write(f"== INFO: Output for {target}\n== ")
+                fd.write(lines)
+                fd.write(f"== INFO: Return code: {proc.returncode}\n\n")
+        subprocess.call(["rm", target])
+
+    print(f"\n    passed tests:    {state.passed_tests}\t")
+    print(f"    failed tests:    {state.failed_tests}\t")
+    print(f"    compile fail:    {state.compile_fail}\t\n")
+
+if __name__ == "__main__":
+    test_nasm_backend()
+    test_native_backend()
